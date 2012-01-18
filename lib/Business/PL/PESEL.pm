9b5040512a9ca6d6d92565123f55bf67b466492a
Tomasz Konojacki <me@xenu.tk>
2012-01-18 21:44:07 +0000

    Import of XENU/Business-PL-PESEL-0.06 from CPAN.
    gitpan-cpan-distribution: Business-PL-PESEL
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         XENU/Business-PL-PESEL-0.06.tar.gz
    gitpan-cpan-author:       XENU
    gitpan-cpan-maturity:     released

--- lib/Business/PL/PESEL.pm
+++ lib/Business/PL/PESEL.pm
@@ -20,7 +20,7 @@
 
 package Business::PL::PESEL;
 
-our $VERSION = '0.05'
+our $VERSION = '0.06';
 
 use strict;
 use warnings;
@@ -112,7 +112,7 @@ of the Population ID
 
 =head1 VERSION
 
-version 0.05
+version 0.06
 
 =head1 SYNOPSIS
 
